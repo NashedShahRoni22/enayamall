e062020f4498f568875ec3cd5d216b9c824043d2
Sayedur Rahman <mrahmansayed@gmail.com>
2025-09-20 01:38:04 +0600

    marquee color update

--- app/components/home/Features.jsx
+++ app/components/home/Features.jsx
@@ -15,15 +15,15 @@ export default function Features() {
   ];
 
   return (
-    <section className="py-[10px] lg:py-[20px] bg-[#ece3dc]">
+    <section className="py-[10px] lg:py-[20px] bg-[#52a5dd26]">
       <Marquee
         autoFill
         speed={25}
       >
         {features.map((feature, index) => (
           <div key={index} className="flex gap-[10px] lg:gap-[20px] items-center">
-            <h5 className={`text-[14px] lg:text-[18px] dirham-symbol items-center flex ${index === 0 && "font-semibold uppercase"} text-center text-black`}>{feature.title}</h5>
-            <div className="size-[3px] lg:size-[6px] bg-black rounded-full mx-[25px] lg:mx-[50px]"></div>
+            <h5 className={`text-[16px] lg:text-[20px] dirham-symbol items-center flex ${index === 0 && "font-semibold uppercase"} text-center text-brand-pink`}>{feature.title}</h5>
+            <div className="size-[3px] lg:size-[6px] bg-brand-pink rounded-full mx-[25px] lg:mx-[50px]"></div>
           </div>
         ))}
       </Marquee>
